b9adaaa29402305709322bb2ca861531c4daed5a
Alexandre Piveteau <alexandre@piveteau.email>
2020-05-15 09:49:55 +0200

    Refactor Color enum
    Bad smell: duplicate code
    Refactoring: extract method & pull up field

--- src/main/java/ch/heigvd/gen2019/Color.java
+++ src/main/java/ch/heigvd/gen2019/Color.java
@@ -1,20 +1,18 @@
 package ch.heigvd.gen2019;
 
 public enum Color {
-  BLUE {
-    @Override
-    public String toString() {
-      return "blue";
-    }
-  }, RED {
-    @Override
-    public String toString() {
-      return "red";
-    }
-  }, YELLOW {
-    @Override
-    public String toString() {
-      return "yellow";
-    }
+  BLUE("blue"),
+  RED("red"),
+  YELLOW("yellow");
+
+  private final String name;
+
+  /* private */ Color(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return this.name;
   }
 }
