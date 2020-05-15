934c1e8d2a05e18feadf357ebca84e18507d9075
Alexandre Piveteau <alexandre@piveteau.email>
2020-05-15 09:44:44 +0200

    Refactor color code
    Bad smell : switch statement
    Refactoring : replace conditional with polymorphism

--- src/main/java/ch/heigvd/gen2019/Product.java
+++ src/main/java/ch/heigvd/gen2019/Product.java
@@ -4,12 +4,12 @@ public class Product {
 
   public static final int SIZE_NOT_APPLICABLE = -1;
   private String code;
-  private int color;
+  private Color color;
   private int size;
   private double price;
   private String currency;
 
-  public Product(String code, int color, int size, double price, String currency) {
+  public Product(String code, Color color, int size, double price, String currency) {
     this.code = code;
     this.color = color;
     this.size = size;
@@ -21,7 +21,7 @@ public class Product {
     return code;
   }
 
-  public int getColor() {
+  public Color getColor() {
     return color;
   }
 
