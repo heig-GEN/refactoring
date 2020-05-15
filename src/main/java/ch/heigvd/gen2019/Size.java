a81fadf8b88f5d624db27ee920d2fa7f313d5aee
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-15 10:04:17 +0200

    Refactor Size toString
    Bad smell: duplicate code
    Refactoring: extract method & pull up field

--- src/main/java/ch/heigvd/gen2019/Size.java
+++ src/main/java/ch/heigvd/gen2019/Size.java
@@ -1,35 +1,16 @@
 package ch.heigvd.gen2019;
 
 public enum Size {
-  XS {
-    @Override
-    public String toString() {
-      return "XS";
-    }
-  }, S {
-    @Override
-    public String toString() {
-      return "S";
-    }
-  }, M {
-    @Override
-    public String toString() {
-      return "M";
-    }
-  }, L {
-    @Override
-    public String toString() {
-      return "L";
-    }
-  }, XL {
-    @Override
-    public String toString() {
-      return "XL";
-    }
-  }, XXL {
-    @Override
-    public String toString() {
-      return "XXL";
-    }
+  XS("XS"), S("S"), M("M"), L("L"), XL("XL") , XXL("XXL");
+
+  private final String name;
+
+  /* private */ Size(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return this.name;
   }
 }
