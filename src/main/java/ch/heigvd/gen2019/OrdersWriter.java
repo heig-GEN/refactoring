0665bcedac56ffe4f6404ca2c92c5b8aa748f70c
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-15 11:14:03 +0200

    Refactor OrderWriter internal for loop
    Code smell: long method
    Refactoring: extract method

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -21,26 +21,7 @@ public class OrdersWriter {
       for (int j = 0; j < order.getProductsCount(); j++) {
         Product product = order.getProduct(j);
 
-        sb.append("{");
-        sb.append("\"code\": \"");
-        sb.append(product.getCode());
-        sb.append("\", ");
-        sb.append("\"color\": \"");
-        sb.append(product.getColor());
-        sb.append("\", ");
-
-        if (product.getSize() != null) {
-          sb.append("\"size\": \"");
-          sb.append(product.getSize());
-          sb.append("\", ");
-        }
-
-        sb.append("\"price\": ");
-        sb.append(product.getPrice());
-        sb.append(", ");
-        sb.append("\"currency\": \"");
-        sb.append(product.getCurrency());
-        sb.append("\"}, ");
+        appendProductTo(sb, product);
       }
 
       if (order.getProductsCount() > 0) {
@@ -56,5 +37,28 @@ public class OrdersWriter {
     }
 
     return sb.append("]}").toString();
+  }
+
+  private void appendProductTo(StringBuffer sb, Product product) {
+    sb.append("{");
+    sb.append("\"code\": \"");
+    sb.append(product.getCode());
+    sb.append("\", ");
+    sb.append("\"color\": \"");
+    sb.append(product.getColor());
+    sb.append("\", ");
+
+    if (product.getSize() != null) {
+      sb.append("\"size\": \"");
+      sb.append(product.getSize());
+      sb.append("\", ");
+    }
+
+    sb.append("\"price\": ");
+    sb.append(product.getPrice());
+    sb.append(", ");
+    sb.append("\"currency\": \"");
+    sb.append(product.getCurrency());
+    sb.append("\"}, ");
   }
 }
