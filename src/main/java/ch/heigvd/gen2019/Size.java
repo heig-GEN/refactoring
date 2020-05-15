26d89c1300b15ac2f33fb37b7538272ac6c55c67
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-15 10:07:10 +0200

    Refactor Size to use provided toString
    Bad smell: primitive obsession
    Refactoring: replace data value with object

--- src/main/java/ch/heigvd/gen2019/Size.java
+++ src/main/java/ch/heigvd/gen2019/Size.java
@@ -1,16 +1,5 @@
 package ch.heigvd.gen2019;
 
 public enum Size {
-  XS("XS"), S("S"), M("M"), L("L"), XL("XL") , XXL("XXL");
-
-  private final String name;
-
-  /* private */ Size(String name) {
-    this.name = name;
-  }
-
-  @Override
-  public String toString() {
-    return this.name;
-  }
+  XS, S, M, L, XL , XXL;
 }
