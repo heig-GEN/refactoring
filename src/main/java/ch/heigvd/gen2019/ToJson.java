92d0499d5bab86c608c98803022260c7e8e3d789
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-05-20 11:56:39 +0200

    Refactor field to json
    Bad smell: duplicate code
    Refactoring: extract method

--- src/main/java/ch/heigvd/gen2019/ToJson.java
+++ src/main/java/ch/heigvd/gen2019/ToJson.java
@@ -17,4 +17,14 @@ public interface ToJson {
     }
     sb.append("]");
   }
+
+  static void appendFieldTo(StringBuffer sb, String fieldName, String fieldValue) {
+    sb.append("\"");
+    sb.append(fieldName);
+    sb.append("\": ");
+
+    sb.append("\"");
+    sb.append(fieldValue);
+    sb.append("\", ");
+  }
 }
